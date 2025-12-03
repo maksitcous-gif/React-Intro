d09fb0d1c646ef05b528099df6a2c1cc4a27e8ad
maksitcous-gif <maksitcous@gmail.com>
2025-12-03 08:37:59 +0000

    Updates

--- vite-prosjekt/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p>©{new Date().getFullYear()} My website.</p>
-        </footer>
-    );
-}
-
-export default Footer;
